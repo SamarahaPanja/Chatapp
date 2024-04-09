53c252b68f5b631690edd0ba07283c4dba5952b0
Sutanjoy Pal <sutanjoypal04@gmail.com>
2024-04-09 23:40:03 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 
 import './App.css';
 
+const Person = (props) => {
+  return(
+    <>
+    <h1>First Name: {props.name}</h1>
+    <h1>Last Name: Pal</h1>
+    <h1>Age: 30</h1>
+    </>
+  )
+}
+
 const App = () => {
+  const name = "Sutanjoy"
+
   return (
     <div className="App">
-      <h1>Hello, React!</h1>
+      <Person name={"Konichiwa"}/>
+      <h1>Hello, {name}!</h1>
+      <h2>{321*261}</h2>
+      {
+        !name ? (
+          <>test</>
+        ) : (
+          <>
+          <h1>TEST</h1>
+          <h2>TEST2</h2>
+          </>
+          
+        )
+      }
+
     </div>
   );
 }
