57fa12ea6d0e24d511e2324bd572b2e6811a0741
Sutanjoy Pal <sutanjoypal04@gmail.com>
2024-04-11 00:39:06 +0530

    4th commit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,44 @@
-
+import { useState } from 'react';
 import './App.css';
+//import { div } from 'three/examples/jsm/nodes/Nodes.js';
 
-const Person = (props) => {
-  return(
-    <>
-    <h1>First Name: {props.name}</h1>
-    <h1>Last Name: Pal</h1>
-    <h1>Age: 30</h1>
-    </>
-  )
-}
+// const Person = (props) => {
+//   return(
+//     <>
+//     <h1>First Name: {props.name}</h1>
+//     <h1>Last Name: Pal</h1>
+//     <h1>Age: 30</h1>
+//     </>
+//   )
+// }
 
 const App = () => {
-  const name = "Sutanjoy"
+  //const name = "Sutanjoy"
+  const [counter , setCounter] = useState(0);
 
   return (
-    <div className="App">
-      <Person name={"Konichiwa"}/>
-      <h1>Hello, {name}!</h1>
-      <h2>{321*261}</h2>
-      {
-        !name ? (
-          <>test</>
-        ) : (
-          <>
-          <h1>TEST</h1>
-          <h2>TEST2</h2>
-          </>
+    // <div className="App">
+    //   <Person name={"Konichiwa"}/>
+    //   <h1>Hello, {name}!</h1>
+    //   <h2>{321*261}</h2>
+    //   {
+    //     !name ? (
+    //       <>test</>
+    //     ) : (
+    //       <>
+    //       <h1>TEST</h1>
+    //       <h2>TEST2</h2>
+    //       </>
           
-        )
-      }
+    //     )
+    //   }
 
+    // </div>
+
+    <div className='App'>
+      <button onClick={() => setCounter((prevCount) => prevCount-1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount+1)}>+</button>
     </div>
   );
 }
