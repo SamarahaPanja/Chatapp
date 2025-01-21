dfa8172935139d25da55a3721d10afaf8c0091e7
Sutanjoy Pal <sutanjoypal04@gmail.com>
2025-01-21 18:20:59 +0530

    Add Chat Feature

--- index.js
+++ index.js
@@ -1,26 +1,104 @@
 const http = require('http')
 const express = require('express')
+const fs = require('fs');
 
 const app = express()
 const server = http.createServer(app);
 const path = require('path')
 
-const {Server} = require('socket.io')
+const { Server } = require('socket.io')
 const io = new Server(server)
 
+app.set("view engine", "ejs")
+app.set("views", path.resolve("./views"))
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+
+
+
+const rooms = {}
+
 io.on('connection', (socket) => {
-    console.log('a user connected',socket.id)
-    socket.on('sendMessage',(message)=>{
-        console.log(message);
-        io.emit('message', message)
+    console.log('a user connected', socket.id)
+    socket.on('file-meta',(data)=>{
+        console.log('file-meta',data)
+        socket.join(data.room)
+        socket.in(data.room).emit('fs-meta',data.metadata)
     })
+    socket.on('fs-start',(data)=>{
+        console.log('fs-start',data)
+        socket.in(data.room).emit('fs-share-sender',{})
+    })
+    socket.on('file-raw',(data)=>{
+        socket.in(data.room).emit('fs-share',data.buffer)
+    })
+    socket.on('new-user', (room, name) => {
+        socket.join(room)
+        rooms[room].users[socket.id] = name
+        socket.to(room).emit('user-connected', name)
+    })
+    socket.on('disconnect', () => {
+        //console.log('user disconnected',socket.id)
+        getUserRooms(socket).forEach(room => {
+            socket.to(room).emit('user-disconnected', rooms[room].users[socket.id])
+            delete rooms[room].users[socket.id]
+        })
+    })
+    socket.on('sendMessage', (room, message) => {
+        //console.log(message);
+
+        //io.emit('message', {message:message,name:users[socket.id]})
+        //socket.to(room).emit('message', { message: message, name: rooms[room].users[socket.id] }) 
+        if (rooms[room]) {
+            // Get the user's name from the rooms object
+            const userName = rooms[room].users[socket.id];
+
+            if (userName) {
+                // Send the message to the room
+                socket.join(room)
+                io.to(room).emit('message', { message: message, name: userName });
+            } else {
+                console.log('User not found in room:', socket.id);
+            }
+        } else {
+            console.log('Room does not exist:', room);
+        }
+    })
+
 })
 
+function getUserRooms(socket) {
+    return Object.entries(rooms).reduce((names, [name, room]) => {
+        if (room.users[socket.id] != null) names.push(name)
+        return names;
+    }, []);
+}
 server.listen(9000, () => console.log('Server started at port 9000'))
 
-app.use(express.static(path.resolve("./public")));
+//app.use(express.static(path.resolve("./public")));
 
-app.get('/', (req, res) =>{
-    return res.sendFile('/public/index.html')
+
+app.get('/', (req, res) => {
+    //return res.sendFile('/public/index.html')
+
+    return res.render('index', { rooms: rooms })
+})
+
+app.post('/room', (req, res) => {
+    if (rooms[req.params.room] != null) {  //case when room already exsists
+        return res.redirect('/')
+    }
+    rooms[req.body.room] = { users: {} };
+    //send message that new room was created
+    io.emit('room-created', req.body.room)
+    res.redirect(req.body.room)
+    
+})
+
+app.get('/:room', (req, res) => {
+    if (rooms[req.params.room] == null) {
+        return res.redirect('/')  //someone tries to access non exsisting room
+    }
+    res.render('room', { roomName: req.params.room })
 })
 
---
+++ public/script.js
@@ -0,0 +1,156 @@
+
+
+function createConnection() {
+
+
+}
+const socket = io();
+const sendBtn = document.getElementById("sendBtn");
+const msgInput = document.getElementById("message");
+const allMessages = document.getElementById("messages");
+const roomContainer = document.getElementById("room-container");
+
+const fileInput = document.getElementById('fileInput');
+const uploadBtn = document.getElementById('uploadBtn');
+const fileStatus = document.getElementById('fileStatus');
+
+if (msgInput != null) {
+    const name = prompt("What is your name?");
+    const p = document.createElement("p");
+    p.innerText = 'You Joined';
+    allMessages.appendChild(p);
+    socket.emit('new-user', roomName, name)
+    
+    sendBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+        const message = msgInput.value;
+        socket.emit("sendMessage", roomName, message);
+        msgInput.value = "";
+    });
+    uploadBtn.addEventListener('click', (e) => {
+        e.preventDefault(); // Prevent any default behavior if necessary
+        const file = fileInput.files[0]; // Get the selected file
+        if (!file) {
+            console.log("No file selected");
+            return;
+        }
+
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            const buffer = new Uint8Array(reader.result);
+            console.log("File content as buffer:", buffer);
+
+            // Send the file data to the server
+            // socket.emit("upload-file", {
+            //     fileName: file.name,
+            //     fileData: buffer
+            // });
+            let el = document.createElement("div");
+            el.classList.add("item")
+            el.innerHTML = `
+                <div class="progress">0%</div>
+                <div class="file-name">${file.name}</div>
+                <button class="download>Download</button>
+            `;
+            allMessages.appendChild(el);
+            shareFile({
+                fileName: file.name,
+                total_buffer_size: buffer.length,
+                buffer_size: 1024
+            }, buffer, el.querySelector(".progress"));
+
+        };
+        reader.readAsArrayBuffer(file);
+    });
+}
+
+function shareFile(metadata, buffer, progress_node) {
+    console.log("started file share");
+    socket.emit("file-meta", {
+        metadata: metadata,
+        room: roomName
+    })
+    console.log("meta data sent");
+    socket.on("fs-share-sender", () => {
+        let chunk = buffer.slice(0, metadata.buffer_size);
+        buffer = buffer.slice(metadata.buffer_size, buffer.length);
+        progress_node.innerText = Math.trunc((metadata.total_buffer_size - buffer.length) / metadata.total_buffer_size * 100) + "%";
+        console.log("sending actual data", chunk.length, buffer.length);
+        if (chunk.length != 0) {
+            socket.emit("file-raw", {
+                room: roomName,
+                buffer: chunk
+            })
+        }
+    })
+}
+
+socket.on('room-created', (room) => {
+    if (roomContainer) {
+        const roomElement = document.createElement("div");
+        roomElement.innerText = room;
+        const roomLink = document.createElement("a");
+        roomLink.innerText = 'Join';
+        roomLink.href = `/${room}`;
+        roomContainer.append(roomElement);
+        roomContainer.append(roomLink);
+    }
+
+})
+
+socket.on('user-connected', (name) => {
+    //console.log(name+' joined the chat')
+    const p = document.createElement("p");
+    p.innerText = `${name} joined the chat`;
+    allMessages.appendChild(p);
+});
+
+socket.on('user-disconnected', (name) => {
+    const p = document.createElement("p");
+    p.innerText = `${name} left the chat`;
+    allMessages.appendChild(p);
+});
+
+socket.on('message', (data) => {
+    console.log(data.message)
+    const p = document.createElement("p");
+    p.innerText = `${data.name} : ${data.message}`
+    allMessages.appendChild(p);
+})
+
+let fileshare = {}
+socket.on("fs-meta", (metadata) => {
+    console.log("received file meta data", metadata);
+    fileshare.metadata = metadata
+    fileshare.transmitted = 0
+    fileshare.buffer = []
+
+    let el = document.createElement("div");
+    el.classList.add("item")
+    el.innerHTML = `
+                <div class="progress">0%</div>
+                <div class="file-name">${metadata.fileName}</div>
+            `;
+    allMessages.appendChild(el);
+    fileshare.progress_node = el.querySelector(".progress")
+    socket.emit("fs-start", {
+        room: roomName
+    })
+    console.log("requesting actual data");
+})
+
+socket.on("fs-share", (buffer) => {
+    console.log(fileshare)
+    fileshare.buffer.push(buffer)
+    fileshare.transmitted += buffer.byteLength;
+    fileshare.progress_node.innerText = Math.trunc(fileshare.transmitted / fileshare.metadata.total_buffer_size * 100) + "%";
+    if (fileshare.transmitted == fileshare.metadata.total_buffer_size) {
+        download(new Blob(fileshare.buffer), fileshare.metadata.fileName);
+        fileshare = {};
+    }
+    else {
+        socket.emit("fs-start", {
+            room: roomName
+        })
+    }
+})
